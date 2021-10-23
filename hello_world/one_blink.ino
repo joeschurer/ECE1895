650dab9eb75c60f9d76af31af3b8bfc53405c4f0
Joseph Schurer <jwschurer@gmail.com>
2021-10-23 13:21:54 -0400

    Adding the files from the hello world assignment.

---
+++ hello_world/one_blink.ino
@@ -0,0 +1,15 @@
+void setup() {
+  pinMode(9, OUTPUT);  
+  pinMode(7, INPUT);
+}
+
+void loop() {
+  if(digitalRead(7) == HIGH){
+    digitalWrite(9, HIGH); 
+    delay(1000);           
+    digitalWrite(9, LOW);  
+    delay(1000);            
+  } else {
+    digitalWrite(9, LOW); 
+  }
+}
---
+++ hello_world/three_blink.ino
@@ -0,0 +1,25 @@
+void setup() {
+  pinMode(9, OUTPUT);   
+  pinMode(7, INPUT);
+  pinMode(8,OUTPUT);
+  pinMode(10,OUTPUT);
+  
+}
+
+void loop() {
+  if(digitalRead(7) == HIGH){
+    digitalWrite(8,HIGH);
+    delay(500);
+    digitalWrite(8,LOW);
+    digitalWrite(9, HIGH); 
+    delay(500);            
+    digitalWrite(9, LOW);
+    digitalWrite(10,HIGH);
+    delay(500);
+    digitalWrite(10,LOW);           
+  } else {
+    digitalWrite(8, LOW);
+    digitalWrite(9, LOW);  
+    digitalWrite(10, LOW);  
+  }
+}

1a6047b38ce4e2af14275077608f046cb435f462
Joe Schurer <jwschurer@gmail.com>
2021-10-31 23:34:36 -0400

    Update LCD_test.ino

--- LCD_test.ino
+++ LCD_test.ino
@@ -1,49 +1,131 @@
+#include <SparkFun_Qwiic_Twist_Arduino_Library.h>
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
+//#include <MPU6050.h>
 
+//setup LCD and gyro
 LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 Adafruit_MPU6050 mpu;
+TWIST twist;
 
+//game variables
 char shake_message[] = "Shake it!";
 char pour_message[] = "Pour it!";
 char twist_message[] = "Twist it!";
-
-#define ROLL_THRESHOLD  20
-
 float X, Y, Z, totalAccel;
-void setup()
-{
-  lcd.init();                      // initialize the lcd 
+int currTask = 0;//0 place, 1 shake, 2 stir
+int oldPos = 0;
+int tasks[3] = {-1,-1,-1};
+int score = 0;
+int started = -1;
 
-  // Print a message to the LCD.
+#define ROLL_THRESHOLD  15
+
+void setup() {
+  pinMode(8, OUTPUT);
+  pinMode(12, INPUT);
+  digitalWrite(12,HIGH);//make pull up
+  pinMode(10 , OUTPUT);
+
+  //LCD Setup
+  lcd.init();
   lcd.backlight();
   lcd.setCursor(1,0);
+  lcd.clear();
+  //lcd.print("TEST");
 
-  Serial.begin(115200);
-
+  //Detect gyro
   while (!mpu.begin()) {
-    Serial.println("MPU6050 not connected!");
+   // Serial.println("MPU6050 not connected!");
+    lcd.print("mstuck");
     delay(1000);
   }
-  Serial.println("MPU6050 ready!");
+
+  //setup twist
+  while(twist.begin()==false){
+    lcd.print("tstuck");
+    delay(1000);
+  }
+  
+  lcd.clear();
+  //Serial.println("MPU6050 ready!");
+  
+  
 }
 sensors_event_t event;
 
-void loop()
-{
-  /*
-  lcd.setCursor(1,0);
-  lcd.print(shake_message);
 
-  delay(1000);
-  lcd.clear();
+void loop() {
+  //detect start switch
+  //delay(5000);
+  if(started == -1){
+    if(digitalRead(12) == LOW){
+      lcd.print("working");
+      delay(1000);
+      lcd.clear();
+      //tone(10,1000,20);
+      //digitalWrite(8,HIGH);
+      started = 1;
+    }
+  }
 
-  lcd.setCursor(1,0);
-  lcd.print(pour_message);
+  if(started == 1){
+    
+    //digitalWrite(8,HIGH);
+    //lcd.clear();
+    //lcd.print("entered");
+    X = 0;
+    Y = 0;
+    Z = 0;
+    for (int i=0; i<10; i++) {
+      mpu.getAccelerometerSensor()->getEvent(&event);
+      X += event.acceleration.x;
+      Y += event.acceleration.y;
+      Z += event.acceleration.z;
+      delay(1);
+    }
+    X /= 10;
+    Y /= 10;
+    Z /= 10;
   
-  delay(1000);
+    totalAccel = sqrt(X*X + Y*Y + Z*Z);
+    
+    Z = event.acceleration.z;
+
+    //test total accel
+    /*lcd.print(totalAccel);
+    delay(1000);
+    lcd.clear();
+    */
+
+    //test twist count
+    lcd.print(twist.getDiff());
+    delay(1000);
+    lcd.clear();
+    
+    
+    if(Z<-6){
+      lcd.setCursor(1,0);
+      lcd.print("upside down");
+      tone(10,1000,20);
+      delay(1000);
+      lcd.clear();
+      digitalWrite(8,HIGH);
+    }
+  
+    if (totalAccel > ROLL_THRESHOLD) {
+      //Serial.println("Shaking");
+      lcd.setCursor(1,0);
+      lcd.print("Shaking");
+      delay(1000);
+      lcd.clear();
+    }
+  
+  }
+  
+}
   lcd.clear();
 
   lcd.setCursor(1,0);
